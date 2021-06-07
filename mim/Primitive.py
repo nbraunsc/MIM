745e1ab391a5e65ec8878549766f4bbd6bc31f06
nbraunsc <nbraunsc@vt.edu>
2021-06-07 18:04:04 -0400

    module for docs

--- mim/Primitive.py
+++ mim/Primitive.py
@@ -1,9 +1,10 @@
 import numpy as np
 
 class Primitive():
+    """ Class to store individual primitives each with their own charge and spin multiplicity, 
+    which atoms are in primitive and label of primitive.
+    
     """
-    Class to store individual primitives each with their own charge and spin multiplicity, which atoms are in primitive and label of primitive 
-"""
 
     def __init__(self, prim_label, atoms, charge=0, spin=1):
         self.prim_label = prim_label
