8090da53cf490e41be168c1e831d0a47d6e376cf
nbraunsc <nbraunsc@vt.edu>
2021-06-08 16:03:28 -0400

    fail safe in run_opt.py

--- inputs/run_opt.py
+++ inputs/run_opt.py
@@ -15,20 +15,63 @@ for i in batch:
         entry = "fragment" + i + ".dill"
         batch_list.append(entry)
 
-print("Batch list:", batch_list)
+#print("Batch list:", batch_list)
 os.chdir(folder)
 os.chdir(directory)
 for i in batch_list:
     #undill and run e, g, hess, apt etc
     infile = open(i, 'rb')
     new_class = dill.load(infile)
-    e, g, h = new_class.qc_backend()
-    new_class.hess_apt(h)  #this should only be done at optimized geometry
+    outfile = open(i, "wb")
+    
+    try:
+        e, g, h = new_class.qc_backend()
+        new_class.hess_apt(h)  #this should only be done at optimized geometry
+    
+    except:
+        print("Job died:", i)
+        name = i.replace("fragment", "").replace(".dill", "_")
+        filename = i.replace(".dill", ".py")
+        submit_line ='sbatch %s -J %s -o "%s" --export=LEVEL="%s",BATCH="%s",FOLDER="%s"  slurm_hess_apt.sh'%(name+"redo", directory+"/"+name+"redo.log", directory, name, folder)     ##For TinkerCliffs/Huckleberry
+        lines = """import os
+import sys
+import dill
+
+old_frag = open('{name}', 'rb')
+fragment_obj = dill.load(old_frag)
+
+#make changes to fragment_obj
+# e.g. fragment_obj.qc_class.spin = 0
+
+new_frag = open('{name}', 'wb')
+dill.dump(fragment_obj, new_frag)
+old_frag.close()
+new_frag.close()
+
+cmd = '{submit_line}'
+os.chdir('../../')
+os.system(cmd)
+"""
+        context = {
+        "name":i,
+        "submit_line":submit_line
+        }
+
+        with open(filename, "w") as myfile:
+            myfile.write(lines.format(**context))
+        
+        myfile.close()
+    
+    finally:
+        ##redill with updated fragment e, g, hess, apt, etc
+        dill.dump(new_class, outfile)
+        infile.close()
+        outfile.close()
+    
 
     print("##################################this is from run_opt", e, g.shape)
     infile.close()
     
     ##redill with updated fragment e, g, hess, apt, etc
-    outfile = open(i, "wb")
     dill.dump(new_class, outfile)
     outfile.close()
